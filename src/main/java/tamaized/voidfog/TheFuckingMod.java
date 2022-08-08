5adf25cd489a86bc59f646fc41e1439b8f92ceaa
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2022-08-07 20:54:49 -0600

    the fucking 1.19 port

--- src/main/java/tamaized/voidfog/TheFuckingMod.java
+++ src/main/java/tamaized/voidfog/TheFuckingMod.java
@@ -8,9 +8,10 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.client.event.EntityViewRenderEvent;
+import net.minecraftforge.client.event.ViewportEvent;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
@@ -22,7 +23,6 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.network.NetworkConstants;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.Random;
 import java.util.function.Consumer;
 
 @Mod("voidfog")
@@ -59,11 +59,11 @@ public class TheFuckingMod {
 		final Pair<Config, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Config::new);
 		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, specPair.getRight());
 		Config.INSTANCE = specPair.getLeft();
-		busForge.addListener((Consumer<EntityViewRenderEvent.RenderFogEvent>) event -> {
+		busForge.addListener((Consumer<ViewportEvent.RenderFog>) event -> {
 			if (active || fog < 1F) {
 				float f = 3F;
 				f = f >= event.getFarPlaneDistance() ? event.getFarPlaneDistance() : Mth.clampedLerp(f, event.getFarPlaneDistance(), fog);
-				float shift = (float) ((active ? (fog > 0.25F ? 0.1F : 0.0005F) : (fog > 0.25F ? 0.001F : 0.0001F)) * event.getPartialTicks());
+				float shift = (float) ((active ? (fog > 0.25F ? 0.1F : 0.0005F) : (fog > 0.25F ? 0.001F : 0.0001F)) * event.getPartialTick());
 				if (active)
 					fog -= shift;
 				else
@@ -79,7 +79,7 @@ public class TheFuckingMod {
 				}
 			}
 		});
-		busForge.addListener((Consumer<EntityViewRenderEvent.FogColors>) event -> {
+		busForge.addListener((Consumer<ViewportEvent.ComputeFogColor>) event -> {
 			if (active || color > 0F) {
 				final float[] realColors = {event.getRed(), event.getGreen(), event.getBlue()};
 				for (int i = 0; i < 3; i++) {
@@ -87,7 +87,7 @@ public class TheFuckingMod {
 					final float c = 0;
 					colors[i] = real == c ? c : Mth.clampedLerp(real, c, color);
 				}
-				float shift = (float) (0.1F * event.getPartialTicks());
+				float shift = (float) (0.1F * event.getPartialTick());
 				if (active)
 					color += shift;
 				else
@@ -103,7 +103,7 @@ public class TheFuckingMod {
 				return;
 			if (event.player.level != null && (event.player.getY() <= event.player.level.getMinBuildHeight() + Config.INSTANCE.y.get() || checkForVoidscapeDimension(event.player.level))) {
 				active = true;
-				Random random = event.player.getRandom();
+				RandomSource random = event.player.getRandom();
 				for (int i = 0; i < 15; i++) {
 					Vec3 vec = event.player.position().add(0, random.nextDouble() * 3D, 0).
 							add(new Vec3(random.nextDouble() * 6D, 0D, 0D).yRot((float) Math.toRadians(random.nextInt(360))));
